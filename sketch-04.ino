8bf204d19e6e44fa5ac325751e7d3206cb2dde5b
Leo Javier <info@leojavier.com>
2015-01-04 20:03:05 -0500

    Control acceleration with the proximity sensor

--- sketch-04.ino
+++ sketch-04.ino
@@ -8,9 +8,17 @@ PIN 6 eje rojo Back
 PIN 9 eje rojo Front
 */
 #include <Servo.h>
+#include <Ultrasonic.h>
+
 #define number_of_servos 4 // Constante para definir cantidad de motores
 int servoPins[] ={ 3, 5, 6, 9 }; // Definicion de Pins a usar
 Servo servos[number_of_servos]; // Declaracion de objetos tipo Servo de size Number_of_servos
+
+// Define PINS para la data del sensor de ultra sonido 12(Trigger PIN) 13(Echo PIN)
+#define TRIGGER_PIN  12
+#define ECHO_PIN     13
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 int led = 13;
 
 // Conectando los Servos a los PINS definidos en servoPins array
@@ -20,18 +28,24 @@ void initEngines(){
   }
 }
 
+long radar(){
+  long microsec = ultrasonic.timing();
+  microsec = constrain(microsec, 0, 4000);
+  long distance = map(microsec, 0, 4000,2285,1000);
+  //Serial.print("MS: ");
+  //Serial.print(distance);
+  //Serial.print("\n");
+  return distance;
+}
+
 void setup(){
-  //Serial.begin(9600);
+  Serial.begin(9600);
   initEngines();
   delay(100);
-  pinMode(led, OUTPUT);
+  //pinMode(led, OUTPUT);
   for(int i=0; i<number_of_servos; i++) {
     servos[i].writeMicroseconds(1000);
   }
-  delay(1000);
-}
-
-void loop() {
   // Senal de iniciacion. enciende y apaga El LED 3 veces en un periodo de 3 Segundos
   for(int i = 0; i < 3; i++) {
     digitalWrite(led,HIGH);
@@ -39,24 +53,15 @@ void loop() {
     digitalWrite(led,LOW);
     delay(500);
   }
-  // Prueba de motores antes de iniciar el vuelo
-  // Enciende motores por un segundo uno por uno
-  for(int i = 0; i < number_of_servos; i++) {
-    digitalWrite(led,HIGH);
-    servos[i].writeMicroseconds(1330);
-    delay(1000);
-    digitalWrite(led,LOW);
-    servos[i].writeMicroseconds(1000);
-  }
-  //Inicia vuelo por 5 segs
-  for(int i = 0; i < number_of_servos; i++) {
-    servos[i].writeMicroseconds(2000);
-  }
-  delay(5000);
-  //Finalizacion de vuelo
-  for(int i = 0; i < number_of_servos; i++) {
-    servos[i].writeMicroseconds(1000);
-  }
-  delay(5000);
+  delay(1000);
+}
+
+void loop() {
+      long myradar = radar();
+
+         for(int i = 0; i < number_of_servos; i++) {
+            servos[i].writeMicroseconds(myradar);
+         }
+      delay(100);
  }
 
